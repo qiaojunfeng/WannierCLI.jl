8d70d05263d167d6067a9a816200fd52d8f102ba
Documenter.jl <documenter@juliadocs.github.io>
2024-01-29 15:34:32 +0000

    build based on e589acc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WannierCLI","category":"page"},{"location":"#WannierCLI","page":"Home","title":"WannierCLI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WannierCLI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WannierCLI]","category":"page"}]
+}
