5bde208b7659bccc814883459e3f394f16ea50b9
Junfeng Qiao <qiaojunfeng@outlook.com>
2024-01-30 20:09:43 +0100

    Add comments on `JULIA_DEPOT_PATH`

--- deps/build.jl
+++ deps/build.jl
@@ -3,4 +3,9 @@ using WannierCLI
 # See possible options by
 # julia --project deps/build.jl -h
 
+# If you want to use a different depot path, e.g., `~/wjl`, run the following
+# in the Wannier.jl directory:
+# JULIA_DEPOT_PATH=~/wjl julia --project -e 'using Pkg; Pkg.instantiate()'
+# JULIA_DEPOT_PATH=~/wjl julia --project deps/build.jl
+
 WannierCLI.comonicon_install()
